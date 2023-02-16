b229792b4b6901d766fe8d49ff8dc142624b81f1
royal <824259085@qq.com>
2023-02-16 16:16:58 +0800

    redis session does not work

--- test-app-02/src/main/java/org/example/config/SessionConfig.java
+++ test-app-02/src/main/java/org/example/config/SessionConfig.java
@@ -6,15 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.FindByIndexNameSessionRepository;
-import org.springframework.session.data.redis.RedisIndexedSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 7 * 24 * 60 * 60, redisNamespace = "auth:goodscattle")
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 7 * 24 * 60 * 60, redisNamespace = "auth:session")
 public class SessionConfig {
 
     @Autowired
@@ -27,16 +22,4 @@ public class SessionConfig {
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort()));
     }
 
-    @Bean
-    public RedisOperations<String, Object> sessionRedisOperations() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(this.redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        return redisTemplate;
-    }
-    @Bean
-    public FindByIndexNameSessionRepository redisSessionRepository(RedisOperations<String, Object> sessionRedisOperations) {
-        return new RedisIndexedSessionRepository(sessionRedisOperations);
-    }
 }
